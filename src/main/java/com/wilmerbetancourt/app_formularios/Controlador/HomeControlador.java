0d01253e923336d57c11d49d94b30d9a4e6f512e
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-01-25 15:45:02 -0500

    Nuevos cambios creando el formulario

--- src/main/java/com/wilmerbetancourt/app_formularios/Controlador/HomeControlador.java
+++ src/main/java/com/wilmerbetancourt/app_formularios/Controlador/HomeControlador.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LandingControlador {
+public class HomeControlador {
 
-    @GetMapping("/landing")
-    public String holaMundo() {
-        return "pages/landing";
+    @GetMapping("/")
+    public String Home(){
+        return "index";
     }
 }
